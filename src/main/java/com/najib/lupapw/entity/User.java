8abd7c9c503ff29c1ee13a13bf6c7781338b9a47
Zibzeus <82931864+Lastwolf666@users.noreply.github.com>
2022-04-20 13:20:16 +0700

    user

--- src/main/java/com/najib/lupapw/entity/User.java
+++ src/main/java/com/najib/lupapw/entity/User.java
@@ -14,7 +14,7 @@ import java.util.Set;
 
 @Data
 @Entity
-@Table(name = "user")
+@Table(name = "t_user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -62,6 +62,9 @@ public class User implements UserDetails {
 
     @Override
     public boolean isEnabled() {
+        return true;
+    }
+}   public boolean isEnabled() {
         return true;
     }
 }
